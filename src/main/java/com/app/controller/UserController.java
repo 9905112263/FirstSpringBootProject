4b290bb9a37c2ac9d2f35e8fb75fa06d2b483dcc
Arshad.Ansari <ansari.arshad27@gmail.com>
2022-03-02 12:39:49 +0530

    user controller added

---
+++ src/main/java/com/app/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.app.controller;
+
+public class UserController {
+	
+	public String getUser() {
+		
+		return "get user was called";
+	}
+	
+    public String createUser() {
+		
+		return "create user was called";
+	}
+
+    public String updateUser() {
+	
+	return "update user was called";
+   }
+
+   public String deleteUser() {
+	
+	return "delete user was called";
+   }
+
+
+}
