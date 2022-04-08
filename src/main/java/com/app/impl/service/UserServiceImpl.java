bbf4041d620e622a6d7736d35564109a0ec43f3a
Arshad.Ansari <Arshad.Ansari@mjunction.in>
2022-04-08 16:45:30 +0530

    changes commited done

--- src/main/java/com/app/impl/service/UserServiceImpl.java
+++ src/main/java/com/app/impl/service/UserServiceImpl.java
@@ -1,7 +1,10 @@
 package com.app.impl.service;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -43,9 +46,13 @@ public class UserServiceImpl implements UserService{
 		return null;
 	}
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 		// TODO Auto-generated method stub
-		return null;
+		UserEntity userEntity=userRepository.findUserByEmail(email);
+		if(userEntity==null) throw new UsernameNotFoundException(email);
+		
+		
+		return new User(userEntity.getEmail(),userEntity.getEncryptedPassword(),new ArrayList<>());
 	}
 
 }
