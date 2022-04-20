a7e507244cc87dd8be11890fd60f8f7e42b35fad
Arshad.Ansari <Arshad.Ansari@mjunction.in>
2022-04-20 11:53:30 +0530

    chnages done

--- src/main/java/com/app/ws/security/AppProperties.java
+++ src/main/java/com/app/ws/security/AppProperties.java
@@ -3,7 +3,7 @@ package com.app.ws.security;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
-@Component
+
 public class AppProperties {
 
 	private Environment env;
